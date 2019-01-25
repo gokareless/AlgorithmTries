9740341ab973f8e486220922b0158ccd4fe8b83e
myan <myan@softserveinc.com>
2019-01-25 11:46:42 +0200

    - Added template

---
+++ src/main/java/org/gokareless/stu/alg/MergeSort.java
@@ -0,0 +1,13 @@
+package org.gokareless.stu.alg;
+
+public class MergeSort {
+
+    public static void main(String[] args) {
+        int[] array = {2, 7, 9, 11, 22, 43, 1, 14, 60, 1, 43, 42, 34, 6, 5, 4};
+        int[] sorted = sort(array);
+    }
+
+    private static int[] sort(int[] array) {
+        return null;
+    }
+}
